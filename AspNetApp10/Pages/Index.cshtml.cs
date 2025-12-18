8c854e19a1e7b036bf8ececc0d215cb4621a18d4
andyvan27 <andyvan27@gmail.com>
2025-12-18 15:27:58 +1100

    Scaffolded AspNetApp10

---
+++ AspNetApp10/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetApp10.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ AspNetApp10/README.md
@@ -0,0 +1,11 @@
+# Create ASPNet App
+```
+dotnet new webapp -n MyAspNetApp10 -f net10.0
+```
+# Run
+```
+dotnet run
+```
+
+# Debug
+F5 | C# |
